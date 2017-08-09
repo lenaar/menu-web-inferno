826fe1c477a573f3811bbb89f8a5d63112a9cd95
Elena Rakhimova <elenara@kth.se>
2017-08-09 15:01:22 +0200

    List to schedule is added

--- public/js/app/pages/SchedulePage.jsx
+++ public/js/app/pages/SchedulePage.jsx
@@ -4,9 +4,10 @@ import Component from 'inferno-component'
 import Container from 'inferno-bootstrap/lib/Container.jsx'
 import Row from 'inferno-bootstrap/lib/Row.jsx'
 import Col from 'inferno-bootstrap/lib/Col.jsx'
-import Nav from 'inferno-bootstrap/lib/Navigation/Nav.jsx'
-import NavItem from 'inferno-bootstrap/lib/Navigation/NavItem.jsx'
-import NavLink from 'inferno-bootstrap/lib/Navigation/NavLink.jsx'
+import ListGroup from 'inferno-bootstrap/lib/List/ListGroup.jsx'
+import ListGroupItem from 'inferno-bootstrap/lib/List/ListGroupItem.jsx'
+import ListGroupItemHeading from 'inferno-bootstrap/lib/List/ListGroupItemHeading.jsx'
+import ListGroupItemText from 'inferno-bootstrap/lib/List/ListGroupItemText.jsx'
 
 class SchedulePage extends Component {
   constructor (props) {
@@ -21,34 +22,29 @@ class SchedulePage extends Component {
          <Container>
               <Row>
                 <Col xs="4">
-                  <h3>Måndag 14 augusti</h3>
-                  <dl>
-                    <dt>Tid: 08:00-10:00</dt>
-                    <dd><a href="#https://www.kth.se/social/program/cmast/event/111818-2/">Upprop</a></dd>
-                    <dd>Plats: <a href="#https://www.kth.se/places/room/id/31e731ab-b185-46b4-b0d3-bd2408563a3e">F1</a>, <a href="#https://www.kth.se/places/room/id/5dc45052-b5ce-4e6c-9ca6-7e22f9eee538">F2</a></dd>
-                    <dd><a href="#https://www.kth.se/social/program/cmast/">Civilingenjörsutbildning i maskinteknik (CMAST, 300 hp)</a></dd>
-                  </dl>
-                  <dl>
-                   <dt>Tid: 08:00-10:00</dt>
-                    <dd>Omtenta</dd>
-                    <dd>Plats: <a href="#https://www.kth.se/places/room/id/31e731ab-b185-46b4-b0d3-bd2408563a3e">F1</a>, <a href="#https://www.kth.se/places/room/id/5dc45052-b5ce-4e6c-9ca6-7e22f9eee538">F2</a></dd>
-                    <dd><a href="#https://www.kth.se/social/program/cmast/">Civilingenjörsutbildning i maskinteknik (CMAST, 300 hp)</a></dd>
-                  </dl>
+                
+                <ListGroup>
+                  <ListGroupItemHeading><h3>Måndag 14 augusti</h3></ListGroupItemHeading>
+                  <ListGroupItem>08:00-10:00</ListGroupItem>
+                  <ListGroupItem><a href="#https://www.kth.se/social/program/cmast/event/111818-2/">Upprop</a></ListGroupItem>
+                  <ListGroupItem>Plats: <a href="#https://www.kth.se/places/room/id/31e731ab-b185-46b4-b0d3-bd2408563a3e">F1</a>, <a href="#https://www.kth.se/places/room/id/5dc45052-b5ce-4e6c-9ca6-7e22f9eee538">F2</a></ListGroupItem>
+                  <ListGroupItem><a href="#https://www.kth.se/social/program/cmast/">Civilingenjörsutbildning i maskinteknik (CMAST, 300 hp)</a></ListGroupItem>
+                </ListGroup>
+                <ListGroup>
+                  <ListGroupItem key="1">08:00-10:00</ListGroupItem>
+                  <ListGroupItem key="2"><a href="#https://www.kth.se/social/program/cmast/event/111818-2/">Upprop</a></ListGroupItem>
+                  <ListGroupItem key="3">Plats: <a href="#https://www.kth.se/places/room/id/31e731ab-b185-46b4-b0d3-bd2408563a3e">F1</a>, <a href="#https://www.kth.se/places/room/id/5dc45052-b5ce-4e6c-9ca6-7e22f9eee538">F2</a></ListGroupItem>
+                  <ListGroupItem key="4"><a href="#https://www.kth.se/social/program/cmast/">Civilingenjörsutbildning i maskinteknik (CMAST, 300 hp)</a></ListGroupItem>
+                </ListGroup>
                 </Col>
                 <Col xs="4">
-                  <h3> Tisdag 15 augusti</h3>
-                  <dl>
-                    <dt>Tid: 08:00-10:00</dt>
-                    <dd>Omtenta</dd>
-                    <dd>Plats: <a href="#https://www.kth.se/places/room/id/31e731ab-b185-46b4-b0d3-bd2408563a3e">F1</a>, <a href="#https://www.kth.se/places/room/id/5dc45052-b5ce-4e6c-9ca6-7e22f9eee538">F2</a></dd>
-                    <dd><a href="#https://www.kth.se/social/program/cmast/">Civilingenjörsutbildning i maskinteknik (CMAST, 300 hp)</a></dd>
-                  </dl>
-                  <dl>
-                    <dt>Tid: 08:00-10:00</dt>
-                    <dd>Omtenta</dd>
-                    <dd>Plats: <a href="#https://www.kth.se/places/room/id/31e731ab-b185-46b4-b0d3-bd2408563a3e">F1</a>, <a href="#https://www.kth.se/places/room/id/5dc45052-b5ce-4e6c-9ca6-7e22f9eee538">F2</a></dd>
-                    <dd><a href="#https://www.kth.se/social/program/cmast/">Civilingenjörsutbildning i maskinteknik (CMAST, 300 hp)</a></dd>
-                  </dl>
+                  <ListGroup>
+                    <ListGroupItemHeading><h3> Tisdag 15 augusti</h3></ListGroupItemHeading>
+                    <ListGroupItem>08:00-10:00</ListGroupItem>
+                    <ListGroupItem><a href="#https://www.kth.se/social/program/cmast/event/111818-2/">Upprop</a></ListGroupItem>
+                    <ListGroupItem>Plats: <a href="#https://www.kth.se/places/room/id/31e731ab-b185-46b4-b0d3-bd2408563a3e">F1</a>, <a href="#https://www.kth.se/places/room/id/5dc45052-b5ce-4e6c-9ca6-7e22f9eee538">F2</a></ListGroupItem>
+                    <ListGroupItem><a href="#https://www.kth.se/social/program/cmast/">Civilingenjörsutbildning i maskinteknik (CMAST, 300 hp)</a></ListGroupItem>
+                  </ListGroup>
                 </Col>
               </Row>
             </Container>
