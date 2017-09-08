c57faf0aa7c0a75bc26a679cbef44b6ee24098a1
Elena Rakhimova <elenara@kth.se>
2017-09-08 17:09:46 +0200

    Use card with cardHeader for notifications page

--- public/js/app/pages/NoticesPage.jsx
+++ public/js/app/pages/NoticesPage.jsx
@@ -3,7 +3,9 @@
 import Component from 'inferno-component'
 import {Animated} from 'inferno-animation'
 import Badge from 'inferno-bootstrap/lib/Badge.jsx'
-
+import Card from 'inferno-bootstrap/lib/Card/Card.jsx'
+import CardBlock from 'inferno-bootstrap/lib/Card/CardBlock.jsx'
+import CardHeader from 'inferno-bootstrap/lib/Card/CardHeader.jsx'
 
 class NoticesPage extends Component {
   constructor (props) {
@@ -18,15 +20,34 @@ class NoticesPage extends Component {
             <Badge pill color="danger">10</Badge>
             <hr />
           <h3>Mina notifieringar</h3>
-          <h4>Att göra</h4>
-          <p>2017-08-20 - 2017-09-05 | <a href="#register">Du har registreringar att göra. Registrering är oppen</a></p>
-          <h4>Information markerad som viktig eller brådskande</h4>
-          <h4>Deadlines</h4>
+          <Card className="notices">
+            <CardHeader>Att göra</CardHeader>
+            <CardBlock>
+              <p>2017-08-20 - 2017-09-05 | <a href="#register">Du har registreringar att göra. Registrering är oppen</a></p>
+            </CardBlock>
+          </Card>
+          <Card className="notices">
+            <CardHeader>Information markerad som viktig eller brådskande</CardHeader>
+            <CardBlock>
+              <p>2017-08-20 - 2017-09-05 | <a href="#register">Du har registreringar att göra. Registrering är oppen</a></p>
+            </CardBlock>
+          </Card>
+          <Card className="notices">
+            <CardHeader>Deadlines</CardHeader>
+            <CardBlock>
+              <p>2017-08-20 - 2017-09-05 | <a href="#register">Du har registreringar att göra. Registrering är oppen</a></p>
+            </CardBlock>
+          </Card>
           <h4><a href="#schema">Mitt schema </a><Badge pill color="primary">10</Badge></h4>
             <hr />
-          <h4>Händelsesr</h4>
-          <p>8 juni 18:22 | Algebra och geometri: Inlägg <a href="#comment">"Hej! Jag har hittat felating engelsk terminologi ..."</a> har kommenterats av Rasmus Kaj</p>
-          <p>15 juni 15:13 | Feedback från användare: Inlägg <a href="#comment">"Just nu arbetar vi med att byta server för detta ..."</a> har kommenterats av Rasmus Kaj </p>
+
+          <Card className="notices">
+            <CardHeader>Händelser</CardHeader>
+            <CardBlock>
+              <p>8 juni 18:22 | Algebra och geometri: Inlägg <a href="#comment">"Hej! Jag har hittat felating engelsk terminologi ..."</a> har kommenterats av Rasmus Kaj</p>
+              <p>15 juni 15:13 | Feedback från användare: Inlägg <a href="#comment">"Just nu arbetar vi med att byta server för detta ..."</a> har kommenterats av Rasmus Kaj </p>
+            </CardBlock>
+          </Card>
         </div>
     )
   }
